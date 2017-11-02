0fd6906a67146b951dbbe85a241ce87d72d28468
Anil Kumar <Anilk1sagar@gmail.com>
2017-11-02 21:07:54 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var stars = document.getElementById('stars');
+
+function displayStar() {
+	stars.style.display = 'block';
+	document.body.style.background = '#000';
+}
+
+function hideStar() {
+	stars.style.display = 'none';
+	document.body.style.background = '#3B4757';
+}
